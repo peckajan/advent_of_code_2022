ed655f0af392a755c83ea09b2df481c8efa46942
Jan Pecka <peckaja2@fit.cvut.cz>
2022-12-03 15:07:45 +0100

    03 done

--- 03/03.py
+++ 03/03.py
@@ -33,10 +33,35 @@ To help prioritize item rearrangement, every item type can be converted to a pri
 In the above example, the priority of the item type that appears in both compartments of each rucksack is 16 (p), 38 (L), 42 (P), 22 (v), 20 (t), and 19 (s); the sum of these is 157.
 
 Find the item type that appears in both compartments of each rucksack. What is the sum of the priorities of those item types?
+
+--- Part Two ---
+
+As you finish identifying the misplaced items, the Elves come to you with another issue.
+
+For safety, the Elves are divided into groups of three. Every Elf carries a badge that identifies their group. For efficiency, within each group of three Elves, the badge is the only item type carried by all three Elves. That is, if a group's badge is item type B, then all three Elves will have item type B somewhere in their rucksack, and at most two of the Elves will be carrying any other item type.
+
+The problem is that someone forgot to put this year's updated authenticity sticker on the badges. All of the badges need to be pulled out of the rucksacks so the new authenticity stickers can be attached.
+
+Additionally, nobody wrote down which item type corresponds to each group's badges. The only way to tell which item type is the right one is by finding the one item type that is common between all three Elves in each group.
+
+Every set of three lines in your list corresponds to a single group, but each group can have a different badge item type. So, in the above example, the first group's rucksacks are the first three lines:
+
+vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+
+And the second group's rucksacks are the next three lines:
+
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw
+
+In the first group, the only item type that appears in all three rucksacks is lowercase r; this must be their badges. In the second group, their badge item type must be Z.
+
+Priorities for these items must still be found to organize the sticker attachment efforts: here, they are 18 (r) for the first group and 52 (Z) for the second group. The sum of these is 70.
 """
 
 FILE_PATH = r'C:\Users\SayMyName\Documents\FIT\advent_of_code_2022\03\input.txt'
-#ALPHABET = {'a': 1, 'b': 1, 'c': 1, 'd': 1, 'e': 1, 'f': 1, 'g': 1, 'h': 1, 'i': 1, 'j': 1, 'k': 1, 'l': 1, 'm': 1, 'n': 1, 'o': 1, 'p': 1, 'q': 1, 'r': 1, 's': 1, 't': 1, 'u': 1, 'v': 1, 'w': 1, 'x': 1, 'y': 1, 'z': 1 }
 
 def readFile(file):
     with open(file, 'r') as file:
@@ -52,6 +77,19 @@ def getScore():
         firstHalf = slice(0,len(line)//2)
         secondHalf = slice(len(line)//2, len(line))
         commonCharacters = list(set(line[firstHalf]) & set(line[secondHalf]))
+        for character in commonCharacters:
+            if character.islower():
+                score += ord(character) - 96
+            else:
+                score += ord(character) - 38
+    
+    return score, getScore2(lines)
+
+
+def getScore2(lines):
+    score = 0
+    for i in range(0,len(lines),3):
+        commonCharacters = list(set(lines[i]) & set(lines[i+1]) & set(lines[i+2]))
         for character in commonCharacters:
             if character.islower():
                 score += ord(character) - 96
